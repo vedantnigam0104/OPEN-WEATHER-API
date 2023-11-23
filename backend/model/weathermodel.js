38dab079b179218678bcf41527aa72075031137d
vedantnigam0104 <105799215+vedantnigam0104@users.noreply.github.com>
2023-11-23 18:13:41 +0530

    Update weathermodel.js

--- backend/model/weathermodel.js
+++ backend/model/weathermodel.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
 
+const weatherDataSchema = new mongoose.Schema({
+  location: String,
+  temperature: Number,
+  humidity: Number,
+  feelsLike: Number,
+  windSpeed: Number,
+  // Add more fields as needed
+}, {
+  strict: false, // Allow extra fields
+});
+
+module.exports = mongoose.model('WeatherData', weatherDataSchema);
